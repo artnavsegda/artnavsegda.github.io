822a082bd82bbab503cf0546679ff9fa9e9250dd
Art Navsegda <artnavsegda@gmail.com>
2019-06-29 15:15:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ console.log("hello javascript");
 
 function hello()
 {
-        xhttp.open("GET", "https://artnavsegda.github.io/hello.txt", false);
-        xhttp.send("hello");
-        document.getElementById("demo").innerHTML = xhttp.responseText;
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://artnavsegda.github.io/hello.txt", false);
+  xhttp.send("hello");
 }
